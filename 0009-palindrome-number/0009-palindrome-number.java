aae3b1e21188a9123c3f01d23381f9a3b5529227
Soumen Laha <soumenlaha465@gmail.com>
2026-06-23 12:40:44 +0530

    Time: 4 ms (100%), Space: 46 MB (53.35%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+         if(x<0) return false;
+         if(x%10==0 && x!=0) return false;
+         int rev =0;
+         while(x>rev){ // reverse the number 
+            rev = (rev*10 + x%10);
+            x/=10;
+         }
+         if(x==rev || x==rev/10 ) return true;
+         return false;
+    }
+}
