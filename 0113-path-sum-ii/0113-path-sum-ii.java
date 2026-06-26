be64f08ce7ed74f978b188cdfd83ee613c804127
Soumen Laha <soumenlaha465@gmail.com>
2026-06-26 14:13:35 +0530

    Time: 12 ms (2.88%), Space: 49.4 MB (5.04%) - LeetHub

---
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<Integer> copy(List<Integer> arr){
+        List<Integer> list = new ArrayList<>();
+        for(int ele: arr){
+            list.add(ele);
+        }
+        return list;
+    }
+    public void helper(TreeNode root, int target,List<Integer> arr,List<List<Integer>> ans){
+        if(root==null) return;
+        if(root.left==null && root.right==null){
+            if(root.val==target){
+                arr.add(root.val);
+                ans.add(arr);
+            }
+            return;
+        }
+            arr.add(root.val);
+            List<Integer> arr1 = copy(arr);
+             List<Integer> arr2 = copy(arr);
+            helper(root.left,target-root.val,arr1,ans);
+            helper(root.right,target-root.val,arr2,ans);
+
+    }
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<List<Integer>> ans = new ArrayList<>();
+         List<Integer> arr = new ArrayList<>();
+         helper(root,targetSum,arr,ans);
+         return ans;
+    }
+}
