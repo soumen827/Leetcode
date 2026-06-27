947338355c1bebbbc1ef7dd714e691d211d89392
Soumen Laha <soumenlaha465@gmail.com>
2026-06-27 11:59:38 +0530

    Time: 1 ms (13.31%), Space: 43 MB (84.78%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -1,26 +1,23 @@
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-             List<Integer> ans = new ArrayList<>();
-        if(root==null) return ans;
+        List<Integer> ans = new ArrayList<>();
+        // if(root==null) return ans; // Base case
          
-        ans.addAll (postorderTraversal(root.left));  
-        ans.addAll (postorderTraversal(root.right));
-          ans.add(root.val);
+        // ans.addAll (postorderTraversal(root.left));  
+        // ans.addAll (postorderTraversal(root.right));
+        //   ans.add(root.val);
+        // return ans;
+
+        Stack<TreeNode> st = new Stack<>();
+       if(root!=null) st.push(root);
+        while(st.size()>0){
+            TreeNode top = st.pop();
+            ans.add(top.val);
+              if(top.left!=null) st.push(top.left);
+              if(top.right!=null) st.push(top.right);
+    
+        }
+        Collections.reverse(ans);
         return ans;
-
-    //     List<Integer> ans = new ArrayList<>();
-    //     Stack<TreeNode> st = new Stack<>();
-        
-    //    if(root!=null) st.push(root);
-    //     while(st.size()>0){
-    //         TreeNode top = st.pop();
-    //         ans.add(top.val);
-    //           if(top.left!=null) st.push(top.left);
-    //           if(top.right!=null) st.push(top.right);
-          
-
-    //     }
-    //     Collections.reverse(ans);
-    //     return ans;
     }
 }
