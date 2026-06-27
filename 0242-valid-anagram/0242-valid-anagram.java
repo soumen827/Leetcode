ce4c57f948ee421024b0251d35e54231f5edff7d
Soumen Laha <soumenlaha465@gmail.com>
2026-06-27 13:18:07 +0530

    Time: 19 ms (10.86%), Space: 46.8 MB (9%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        HashMap<Character,Integer> map1 = new HashMap<>();
+         for(int i=0;i<s.length();i++){
+            char key = s.charAt(i);
+            if(map1.containsKey(key)){
+                int freq = map1.get(key);
+                map1.put(key,freq+1);
+            }else map1.put(key,1);
+         }
+
+        
+        HashMap<Character,Integer> map2 = new HashMap<>();
+         for(int i=0;i<t.length();i++){
+            char key = t.charAt(i);
+            if(map2.containsKey(key)){
+            int freq = map2.get(key);
+            map2.put(key,freq+1);
+            }  else map2.put(key,1);
+         }
+
+         for(char key:map1.keySet()){
+            
+            int val1 = map1.get(key);
+            if(!map2.containsKey(key)) return false;
+            int val2 = map2.get(key);
+
+            if(val1!=val2) return false;
+         }
+         return true;
+    }
+}
