41bb7b89877edd1172ed67e6faba25ed9ad81b72
Soumen Laha <soumenlaha465@gmail.com>
2026-06-28 12:40:02 +0530

    Time: 1 ms (38.49%), Space: 43.3 MB (51.26%) - LeetHub

---
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.java
@@ -0,0 +1,36 @@
+class Solution{
+    public int matrixScore(int[][] arr){
+        int m = arr.length, n = arr[0].length;
+        for(int i=0;i<m;i++){
+            if(arr[i][0]==0){ // flip row wise
+                for(int j=0;j<n;j++){
+                  if(arr[i][j]==0) arr[i][j] =1;
+                  else arr[i][j]=0;  
+                }
+            }
+        }
+
+        for(int j=1;j<n;j++){
+            int noOfzero =0, noOfones =0;
+            for(int i =0;i<m;i++){
+                 if(arr[i][j]==0) noOfzero++;
+                 else noOfones++;
+            }
+            if(noOfzero>noOfones){
+                for(int i=0;i<m;i++){
+                      if(arr[i][j]==0) arr[i][j] =1;
+                      else arr[i][j]=0;  
+                }               
+            }
+        }
+        int score =0;
+        int x=1;
+        for(int j=n-1;j>=0;j--){
+            for(int i=0;i<m ;i++){
+                score += (arr[i][j]*x);
+            }
+            x *=2;
+        }
+        return score;
+    }
+}
