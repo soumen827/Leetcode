62547ad52548a392184bbe7e984bc89ac34265df
Soumen Laha <soumenlaha465@gmail.com>
2026-06-28 12:24:57 +0530

    Time: 15 ms (55.42%), Space: 47.7 MB (31.55%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -0,0 +1,33 @@
+class Solution {
+   public class Pair implements Comparable<Pair>{
+    int ele;
+    int freq;
+    Pair(int ele,int freq){
+        this.ele = ele;
+        this.freq = freq;
+    }
+    public int compareTo(Pair s){
+          return this.freq - s.freq;
+    }
+}
+    public int[] topKFrequent(int[] arr, int k) {
+        Map<Integer,Integer> map = new HashMap<>();
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        for(int ele:arr){
+           if(map.containsKey(ele)){
+            map.put(ele,map.get(ele)+1);
+           } else map.put(ele,1);
+        }
+         for(int ele:map.keySet()){
+            int freq = map.get(ele);
+            pq.add(new Pair(ele,freq));
+            if(pq.size()>k) pq.remove();
+         }
+         int[] ans = new int[k];
+         for(int i=0;i<k;i++){
+            Pair p = pq.remove();
+            ans[i] = p.ele;
+         }
+         return ans;
+    }
+}
