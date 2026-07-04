55363b5d16a9573eede3ad5fd301f62bdfbfb963
Soumen Laha <soumenlaha465@gmail.com>
2026-07-04 12:29:00 +0530

    Time: 0 ms (100%), Space: 44.3 MB (35.17%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,10 +1,8 @@
 class Solution {
     public int[] runningSum(int[] arr) {
-        int n= arr.length;
-        int pre[] = new int[n];
-        
+        int n = arr.length;
         for(int i=1;i<n;i++){
-            arr[i] +=arr[i-1];
+            arr[i]+=arr[i-1];
         }
         return arr;
     }
