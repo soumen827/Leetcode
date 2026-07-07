58f4a7346eb75069892fb278e75e02169f05360e
Soumen Laha <soumenlaha465@gmail.com>
2026-07-07 18:59:38 +0530

    Time: 703 ms (19.5%), Space: 85 MB (20.69%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] corpFlightBookings(int[][] arr, int n) {
+        int [] ans = new int[n];
+        for(int i=0;i<arr.length;i++){
+            int first = arr[i][0];
+            int last = arr[i][1];
+            int seat = arr[i][2];
+            for(int j=first-1;j<last;j++){
+                ans[j]+= seat;
+            }
+        }
+        return ans;
+    }
+}
