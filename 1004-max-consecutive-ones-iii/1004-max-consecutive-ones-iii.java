57e64b18f37f07ae7f12a583fb766cd917a5bfed
Soumen Laha <soumenlaha465@gmail.com>
2026-07-16 23:50:48 +0530

    Time: 4 ms (44.59%), Space: 51.8 MB (94.66%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,41 +1,24 @@
 class Solution {
     public int longestOnes(int[] arr, int k) {
-
         int n = arr.length;
-        int i = 0, j = 0, zeros = 0, maxLen = 0;
-        // Build the first valid window
-        while (j < n) {
-            if (arr[j] == 0) {
-                if (zeros == k)
-                    break;
-                zeros++;
-            }
-            j++;
-        }
-        j--;
+        int i = 0;
+        int zeros = 0;
+        int maxLen = 0;
 
-        while (i < n && j < n) {
+        for (int j = 0; j < n; j++) {
+            
+            if (arr[j] == 0)
+                zeros++;
+
+            while (zeros > k) {
+                if (arr[i] == 0)
+                    zeros--;
+                i++;
+            }
 
             maxLen = Math.max(maxLen, j - i + 1);
-
-            // Remove left element
-            if (arr[i] == 0)
-                zeros--;
-            i++;
-
-            // Expand right until window becomes valid again
-            j++;
-            while (j < n) {
-                if (arr[j] == 0) {
-                    if (zeros == k)
-                        break;
-                    zeros++;
-                }
-                j++;
-            }
-            j--;
         }
-        
+
         return maxLen;
     }
 }
