20fe7e62349e53c3c2014f87052ceaa1e018945a
Soumen Laha <soumenlaha465@gmail.com>
2026-07-16 23:53:10 +0530

    Time: 4 ms (44.59%), Space: 52.4 MB (17.87%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -6,19 +6,17 @@ class Solution {
         int maxLen = 0;
 
         for (int j = 0; j < n; j++) {
-            
+
             if (arr[j] == 0)
                 zeros++;
 
-            while (zeros > k) {
+            while (zeros > k) { // same as 1493 only this line change
                 if (arr[i] == 0)
                     zeros--;
                 i++;
             }
-
             maxLen = Math.max(maxLen, j - i + 1);
         }
-
         return maxLen;
     }
 }
