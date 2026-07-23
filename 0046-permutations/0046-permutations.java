9a34627385d965e2f540375d4df774b1988d1397
Soumen Laha <soumenlaha465@gmail.com>
2026-07-24 00:14:23 +0530

    Time: 0 ms (100%), Space: 45.6 MB (28.94%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,30 +1,29 @@
 class Solution {
-    public void helper(int[] nums,List<Integer> ds,boolean[] isValid,List<List<Integer>> ans){
+    public void helper(int[] nums,int idx,List<List<Integer>> ans){
         int n = nums.length;
-        if(ds.size()==n){
-           List<Integer> list = new ArrayList<>();
-           for(int i=0;i<ds.size();i++){
-                list.add(ds.get(i));
-           }
-           ans.add(list);
-           return;
-        }
-        for(int i=0;i<n;i++){
-            if(isValid[i]==false){ // call lagega
-                ds.add(nums[i]);
-                isValid[i]= true;
-                helper(nums,ds,isValid,ans);
-                isValid[i]= false;
-                ds.remove(ds.size()-1);
-
+        if(idx==n-1){
+            List<Integer> l = new ArrayList<>();
+            for(int i=0;i<n;i++){
+                l.add(nums[i]);
             }
+            ans.add(l);
+            return;
+        }
+        for(int i=idx;i<n;i++){
+           swap(i,idx,nums);
+           helper(nums,idx+1,ans);
+           swap(i,idx,nums);
+
         }
     }
+    public void swap(int i,int j, int[] nums){
+        int temp = nums[i];
+        nums[i] =nums[j]; 
+        nums[j] = temp;
+    }
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>(); // 0(n) mandatary
-        List<Integer> ds = new ArrayList<>();  // 0(n)
-        boolean[] isValid = new boolean[nums.length];
-        helper(nums,ds,isValid,ans);
+        List<List<Integer>> ans = new ArrayList<>(); // 0(n) mandatory
+        helper(nums,0,ans);
         return ans;
     }
 }
