7cccd590cc85bce4fa74d1b1593e03f16daade35
Soumen Laha <soumenlaha465@gmail.com>
2026-07-25 17:16:30 +0530

    Time: 1 ms (95.64%), Space: 46.3 MB (71.25%) - LeetHub

---
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean helper(int[][] grid,int row, int col, int num){
+        int n =grid.length;
+        int i,j;
+        // Base case
+        if(grid[row][col]==n*n-1) return true;
+        // 2up left
+        i = row-2;
+        j = col-1;
+        if(i>=0 && j>=0 && grid[i][j]==num+1) return helper(grid,i,j,num+1);
+        // 2up right
+        i = row-2;
+        j = col+1;
+        if(i>=0 && j<n && grid[i][j]==num+1) return helper(grid,i,j,num+1);
+        // 2 down left
+        i = row+2;
+        j = col-1;
+        if(i<n && j>=0 && grid[i][j]==num+1) return helper(grid,i,j,num+1);
+        // 2 down right
+        i = row+2;
+        j = col+1;
+        if(i<n && j<n && grid[i][j]==num+1) return helper(grid,i,j,num+1);
+        // 2 right up
+        i = row-1;
+        j = col+2;
+        if(i>=0 && j<n && grid[i][j]==num+1) return helper(grid,i,j,num+1);
+        // 2 right down
+        i = row+1;
+        j = col+2;
+        if(i<n && j<n && grid[i][j]==num+1) return helper(grid,i,j,num+1);
+        // 2 left up
+        i = row-1;
+        j = col-2;
+        if(i>=0 && j>=0 && grid[i][j]==num+1) return helper(grid,i,j,num+1);
+        // 2 left down
+        i = row+1;
+        j = col-2;
+        if(i<n && j>=0 && grid[i][j]==num+1) return helper(grid,i,j,num+1);
+        return false;
+        
+    }
+    public boolean checkValidGrid(int[][] grid) {
+        if(grid[0][0]!=0) return false;
+        return helper(grid,0,0,0); // row col valu
+    }
+}
