928ba044b8f5fcf95c4ee85724c89ec159ad2851
Soumen Laha <soumenlaha465@gmail.com>
2026-07-29 18:19:52 +0530

    Time: 1 ms (99.96%), Space: 47.1 MB (27.98%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -3,6 +3,7 @@ class Solution {
         int ans=0; // me janta hu sab 2 bar karke hay to ^ karunga to 0 hoga 
         for(int ele:nums){
             ans = ans ^ ele; // only one single bachega
+            //ans^=ele;
         }
         return ans;
     }
