df93c9183ae2eea5311808f68d5bdc6bf545df95
Soumen Laha <soumenlaha465@gmail.com>
2026-07-31 11:04:45 +0530

    Time: 0 ms (100%), Space: 42.7 MB (15.77%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,7 +1,17 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n==1) return true;
-        if(n<=0 || n%4!=0) return false;
-        return isPowerOfFour(n/4);
+        //Brute forse
+        // if(n==1) return true;
+        // if(n<=0 || n%4!=0) return false;
+        // return isPowerOfFour(n/4);
+        
+        // M-2
+        if(n<=0) return false;
+        return ((n&(n-1))==0) && isPerfectSq(n);
+        
+    }
+    public boolean isPerfectSq(int n){
+        int root = (int)Math.sqrt(n);
+        return (root*root==n);
     }
 }
