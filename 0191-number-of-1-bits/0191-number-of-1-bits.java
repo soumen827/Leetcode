040342d1919e19388917e70794966bda5692a3d7
Soumen Laha <soumenlaha465@gmail.com>
2026-07-31 10:37:42 +0530

    Time: 1 ms (31.1%), Space: 42.6 MB (12.06%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,10 +1,17 @@
 class Solution {
     public int hammingWeight(int n) {
+        // Method 1
         int count =0;
-        while(n!=0){
-            n = n& (n-1);
-            count++;
+        // while(n!=0){
+        //     n = n& (n-1);
+        //     count++;
+        // }
+            // method 2 
+        for(int i=0;i<=31;i++){
+            int mask = 1<<i;
+            if((n & mask) != 0) count ++;
         }
         return count;
+
     }
 }
