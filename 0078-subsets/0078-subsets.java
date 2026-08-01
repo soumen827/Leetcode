b43f0051a7638de910dc7e1700870aab32ce4ae0
Soumen Laha <soumenlaha465@gmail.com>
2026-08-01 18:42:07 +0530

    Time: 1 ms (86.85%), Space: 44.3 MB (39.56%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        int total = (1<<n);
+        for(int num =0; num<total;num++){
+            List<Integer> list = new ArrayList<>();
+            for(int bitIdx =0;bitIdx<n;bitIdx++){
+                int mask = (1<<bitIdx);
+                if((num & mask)!=0) list.add(nums[bitIdx]);
+            }
+            ans.add(list);
+        }
+        return ans;
+    }
+}
