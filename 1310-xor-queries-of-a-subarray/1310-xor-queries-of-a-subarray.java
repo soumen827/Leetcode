5054d17c9f1c6e63f78bb6da8382bed709e412d2
Soumen Laha <soumenlaha465@gmail.com>
2026-08-01 23:57:50 +0530

    Time: 2 ms (100%), Space: 98.9 MB (16.38%) - LeetHub

---
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] xorQueries(int[] arr, int[][] queries) {
+        int n = arr.length, m = queries.length;
+        for(int i=1;i<n;i++){
+            arr[i] ^= arr[i-1]; // arr is now prefix
+        }
+        int[] ans = new int[m];
+        for(int i=0;i<m;i++){
+            int left = queries[i][0], right = queries[i][1];
+            if(left==0) ans[i] = arr[right];
+            else ans[i] = arr[left-1] ^ arr[right]; // 1 se b tak XOR - 1 se a-1 tak ka XOR
+        }
+        return ans;
+    }
+}
