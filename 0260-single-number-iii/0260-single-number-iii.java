ddf573aeeebd1f3073b9b262bbe0437e2c9c2bb3
Soumen Laha <soumenlaha465@gmail.com>
2026-08-01 17:25:38 +0530

    Time: 1 ms (100%), Space: 48.1 MB (23.71%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        Map<Integer,Integer> map = new HashMap<>();
+        int xor =0;
+        for(int ele: nums){
+            xor ^=ele;
+        }
+        int RightBitOff = xor & (xor-1);
+        int mask = xor ^ RightBitOff;
+        int b1 =0, b2 =0;
         for(int ele:nums){
-            if(map.containsKey(ele)) map.put(ele,2); // pehelese ak hay to 2 kardo
-            else map.put(ele,1);
+            if((mask & ele)==0) b1 ^= ele;
+            else b2 ^= ele;
         }
-        for(int key:map.keySet()){
-            int freq = map.get(key);
-            if(freq==1) list.add(key);
-        }
-        int[] ans = new int[2];
-        ans[0]= list.get(0); ans[1] = list.get(1); // list se ans me past
+        int[] ans = {b1,b2};
         return ans;
     }
 }
