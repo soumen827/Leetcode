a1a0c11a680ee1da8ff2b62c50bfe9e6e88dde08
Soumen Laha <soumenlaha465@gmail.com>
2026-08-06 23:23:27 +0530

    Time: 0 ms (100%), Space: 42.9 MB (38.24%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,13 +1,14 @@
 class Solution {
     public int rob(int[] cost) {
         int n = cost.length;
-        if(n==1) return cost[0];
+        //if(n==1) return cost[0];
         int[] dp = new int[n];
         dp[0] = cost[0];
-        dp[1] = Math.max(cost[0],cost[1]); // 2 no me se jo bara hay
+        if(n>1) dp[1] = Math.max(cost[0],cost[1]); // 2 no me se jo bara hay
         for(int i=2;i<n;i++){
             dp[i] = Math.max(cost[i]+dp[i-2],dp[i-1]);
         }
-        return Math.max(dp[n-1],dp[n-2]);
+        //return Math.max(dp[n-1],dp[n-2]);
+        return dp[n-1];
     }
 }
