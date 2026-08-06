d8ab5e3699b241278579513873ee5f7b0ca26174
Soumen Laha <soumenlaha465@gmail.com>
2026-08-06 22:27:13 +0530

    Refactor rob function to eliminate dynamic programming
    Removed memoization to simplify the recursion.

--- 0198-house-robber/Using_Recursion.java
+++ 0198-house-robber/Using_Recursion.java
@@ -1,16 +1,14 @@
 class Solution {
+    //TLE problem
     public int rob(int[] nums) {
         int n = nums.length;
-        int []dp = new int[n];
-        Arrays.fill(dp,-1);
-        return amount(nums,0,dp);
+        return amount(nums,0);
     }
-    public int amount(int[] nums,int i,int[] dp) {
+    public int amount(int[] nums,int i) {
         int n = nums.length;
         if(i>=n) return 0;
-        if(dp[i]!=-1) return dp[i];// dp mw already kuch hay
-        int take = nums[i] + amount(nums,i+2,dp);
-        int skip = amount(nums,i+1,dp);
+        int take = nums[i] + amount(nums,i+2);
+        int skip = amount(nums,i+1);
         return dp[i] = Math.max(take,skip);
     }
 }
