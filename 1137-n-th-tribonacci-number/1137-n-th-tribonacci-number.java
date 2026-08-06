5fe47f0f5340dd42c4f3afa26c10a9ffa26b20c6
Soumen Laha <soumenlaha465@gmail.com>
2026-08-06 20:27:24 +0530

    Time: 0 ms (100%), Space: 41.9 MB (72.14%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,16 @@
+
+class Solution {
+    //static int[] dp;
+    public int tribonacci(int n) {
+        int[] dp =new int[n+1];
+        return fibo(n,dp);
+    }
+    public int fibo(int n,int[] dp){
+        if(n==0) return 0;
+        if(n==1 || n==2) return 1;
+        if(dp[n]!=0) return dp[n]; // dp me kuch hay to ohi ans hay
+        int ans = fibo(n-1,dp)+ fibo(n-2,dp)+ fibo(n-3,dp);
+        dp[n] = ans; // ans ko dp me save karo
+        return ans;
+    }
+}
