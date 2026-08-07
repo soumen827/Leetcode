22cc83f58344babaa728e9ae2bd9d8f58bd2e37d
Soumen Laha <soumenlaha465@gmail.com>
2026-08-07 23:59:08 +0530

    Time: 0 ms (100%), Space: 41.5 MB (99.18%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[]dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return way(n,dp);
+    }
+    public int way(int n,int[] dp) {
+        if(n==0 || n==1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]= way(n-1,dp) + way(n-2,dp);
+    }
+}
