1aeea9fed2f943e278860d0b86154b4bed6c439a
Soumen Laha <soumenlaha465@gmail.com>
2026-08-08 19:55:07 +0530

    Time: 1 ms (17.12%), Space: 43.6 MB (27.1%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -3,7 +3,6 @@ class Solution {
         int m = arr.length;
         int n = arr[0].length;
         int[][] dp = new int[m][n];
-
         // Starting cell is an obstacle
         if (arr[0][0] == 1) return 0;
         dp[0][0] = 1; // first ele 0
@@ -11,7 +10,6 @@ class Solution {
             for (int j = 0; j < n; j++) {
                 // Obstacle
                 if (arr[i][j] ==1) dp[i][j] = 0;
-               // else if (i == 0 && j == 0)  dp[i][j] = 1;
                 else {
                     if (i > 0) dp[i][j] += dp[i - 1][j];
                     if (j > 0) dp[i][j] += dp[i][j - 1];
