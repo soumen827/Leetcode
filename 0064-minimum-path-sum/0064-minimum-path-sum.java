5d9beea588b835b6c976a1ec6f07414e2b0b9c3e
Soumen Laha <soumenlaha465@gmail.com>
2026-08-08 19:25:46 +0530

    Time: 4 ms (62.18%), Space: 50.5 MB (21.58%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,16 +1,16 @@
 class Solution {
     public int minPathSum(int[][] arr) {
+        // without using the extra space
         int m = arr.length;
         int n = arr[0].length;
-        int[][] dp = new int[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(i==0 && j==0) dp[i][j] = arr[i][j];//for 1st ele
-                else if(i==0) dp[i][j] = arr[i][j] + dp[i][j-1];//for row changing
-                else if(j==0) dp[i][j] = arr[i][j] + dp[i-1][j];// for col changing
-                else dp[i][j] = arr[i][j]+ Math.min(dp[i][j-1],dp[i-1][j]); // original dp formula
+                if(i==0 && j==0) continue;//for 1st ele
+                else if(i==0) arr[i][j] += arr[i][j-1];//for row changing
+                else if(j==0) arr[i][j] += arr[i-1][j];// for col changing
+                else arr[i][j] += Math.min(arr[i][j-1],arr[i-1][j]); // original dp formula
             }
         }
-        return dp[m-1][n-1];
+        return arr[m-1][n-1];
     }
 }
