de73db60e8b29014b0e83604aab2880a9428812a
Soumen Laha <soumenlaha465@gmail.com>
2026-08-08 11:20:59 +0530

    Time: 0 ms (100%), Space: 42.2 MB (29.54%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,5 +1,8 @@
 class Solution {
+     int m,n;
     public int uniquePaths(int m, int n) {
+        this.m =m;
+        this.n =n;
         int[][] dp = new int[m][n];
         //Arrays.fill(dp,-1);
         for(int i=0;i<m;i++){
@@ -7,14 +10,14 @@ class Solution {
                 dp[i][j] = -1;
             }
         }
-        return helper(0,0,m,n,dp);
+        return helper(0,0,dp);
     }
-    public int helper(int row,int col,int m, int n,int[][]dp) {
+    public int helper(int row,int col,int[][]dp) {
         if(row>=m || col>=n) return 0;
         if(row==m-1 || col==n-1) return 1;
         if(dp[row][col]!=-1) return dp[row][col];
-        int rightway = helper(row,col+1,m,n,dp);
-        int downway = helper(row+1,col,m,n,dp);
+        int rightway = helper(row,col+1,dp);
+        int downway = helper(row+1,col,dp);
         return dp[row][col]= rightway + downway;
     }
 }
